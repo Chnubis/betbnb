1a9162a360f46c2a86a4d4c177c265c5619bbc31
chnubis <Sergej.Kaiserr@gmail.com>
2020-12-28 10:56:32 +0100

    changed logo

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,7 +39,7 @@ const Layout = ({ children }) => {
         }}>
           © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          <a href="https://www.betbnb24.com">Betbnb24</a>
         </footer>
       </div>
     </>
